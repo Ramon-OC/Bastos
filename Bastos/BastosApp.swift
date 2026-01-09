b2419f601bc10bc783bc15fa8905ea5dee550c3e
José Ramón Ortiz Castañeda <joseramonortizcastaneda@MacBook-Pro-de-Jose.local>
2026-01-09 14:00:42 -0600

    My first commit

---
+++ Bastos/BastosApp.swift
@@ -0,0 +1,17 @@
+//
+//  BastosApp.swift
+//  Bastos
+//
+//  Created by José Ramón Ortiz Castañeda on 09/01/26.
+//
+
+import SwiftUI
+
+@main
+struct BastosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bastos/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bastos
+//
+//  Created by José Ramón Ortiz Castañeda on 09/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
